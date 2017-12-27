4888dffac39234f1e719dadf51dd4ddff49fc05f
DKwtno <wei609778992@gmail.com>
2017-12-27 21:11:14 +0800

    回文数

---
+++ basic/huiwen.java
@@ -0,0 +1,14 @@
+
+
+public class huiwen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<10;i++){
+			for(int j=0;j<10;j++){
+				System.out.println(i*1000+j*100+j*10+i);
+			}
+		}
+	}
+
+}
